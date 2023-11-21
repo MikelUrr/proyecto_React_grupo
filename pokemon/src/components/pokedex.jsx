a2cd18ade785afc10ddd8a42b14d0422486684fe
mikel <mikel.urrestarazu@gmail.com>
2023-11-21 17:00:06 +0100

    fetch a la api en pokedex

---
+++ pokemon/src/components/pokedex.jsx
@@ -0,0 +1,16 @@
+import { useState,useEffect } from 'react'
+
+
+const getGenerations = async () => {
+    try {
+      const pokemonData = await fetch('https://pokeapi.co/api/v2/generation/');
+      const result = await pokemonData.json();
+      const generations = result.results; 
+      console.log(generations);
+      setPokemonData(generations);
+    } catch (error) {
+      console.error("Algo ha salido mal...", error);
+      setError("Algo ha salido mal...");
+    }
+  };
+  
