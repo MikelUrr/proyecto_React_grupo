8ec2fbdfd82b659d53acd7c0ce2f66779c11b2da
Aramendiander <aramendiander@gmail.com>
2023-11-21 17:11:52 +0100

    añadida funcionalidad de botones dinámica

---
+++ pokemon/src/components/tablaTipos.jsx
@@ -0,0 +1,19 @@
+// Pokedex.js
+import React, { useState, useEffect } from 'react';
+
+const tablaTipos = () => {
+  const [listaTipos, setListaTipos] = useState([]);
+
+/*   useEffect(() => {
+   }, []); */
+
+  return (
+    <div>
+      <h2>Tablatipos</h2>
+      <p>Hola Mikel</p>
+      {/* Renderiza la lista de Pokémon */}
+    </div>
+  );
+};
+
+export default tablaTipos;
