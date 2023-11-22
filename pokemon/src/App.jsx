3b3c7a2fddb55971dee4f39f0b1c64a291899e5a
mikel <mikel.urrestarazu@gmail.com>
2023-11-22 08:03:44 +0100

    creacion de pokex hasta mostrar pokemon

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+//import TablaTipos from './components/tablaTipos'
+import Pokedex from './components/pokedex'; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeComponent, setActiveComponent] = useState(null);
+
+  const handleTablaTiposClick = () => {
+    setActiveComponent('tablaTipos');
+  };
+  const handlePokedexClick = () => {
+    setActiveComponent('pokedex');
+  };
+
+  const handleHomeClick = () => {
+    setActiveComponent('');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          PEC is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        {activeComponent !== 'tablaTipos' && activeComponent !== 'pokedex' && (
+          <>
+            <h1>Pokemongol</h1>
+            <button onClick={handleTablaTiposClick}>Tabla de Tipos</button>
+            <button onClick={handlePokedexClick}>Pokedex</button>
+          </>
+        )}
+
+        {activeComponent === 'tablaTipos' && ( 
+          <>
+            <button onClick={handleHomeClick}>Atrás</button>
+            <TablaTipos />
+          </>
+        )}
+
+        {activeComponent === 'pokedex' && (
+          <>
+            <button onClick={handleHomeClick}>Atrás</button>
+            <Pokedex />
+          </>
+        )}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ pokemon/src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <h3>{pokemon.name}</h3>
+
+    </div>
+  );
+};
+
+export default PokemonCard;
