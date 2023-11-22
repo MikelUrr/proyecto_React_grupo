4cc4aeb276a4dcf806153d610c09ee19d462d02f
Aramendiander <aramendiander@gmail.com>
2023-11-22 17:26:54 +0100

    seleccionar dos pokimon a cada lado

--- pokemon/src/components/tablaTipos.jsx
+++ pokemon/src/components/tablaTipos.jsx
@@ -2,17 +2,80 @@
 import React, { useState, useEffect } from 'react';
 
 const tablaTipos = () => {
-  const [listaTipos, setListaTipos] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
+  const [error, setError] = useState("");
+  const [currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon/?limit=9999");
+  const [leftPokemon, setLeftPokemon] = useState(null);
+  const [rightPokemon, setRightPokemon] = useState(null);
 
-/*   useEffect(() => {
-   }, []); */
+  useEffect(() => {
+    getPokemons();
+  }, []);
 
+  const getPokemons = async () => {
+    try {
+      const data = await fetch(currentUrl);
+      const results = await data.json();
+      setPokemonList(results.results);
+    } catch (e) {
+      setError("Algo salió mal");
+      console.error(e);
+    }
+  };
+
+  const fetchPokemonDetails = async (pokemonUrl, side) => {
+    try {
+      const data = await fetch(pokemonUrl);
+      const results = await data.json();
+
+      if (side === "left") {
+        setLeftPokemon(results);
+      } else {
+        setRightPokemon(results);
+      }
+    } catch (error) {
+      console.error('Error fetching Pokemon details:', error);
+    }
+  };
+
+
+  
   return (
-    <div>
-      <h2>Tablatipos</h2>
-      <p>Hola Mikel</p>
-      {/* Renderiza la lista de Pokémon */}
-    </div>
+    <>
+      <h1>¿Quién ganará?</h1>
+      <section className="fight">
+        <div className="left">
+          <ul>
+            {pokemonList.map((pokemon, index) => (
+              <li key={index}>
+                <button onClick={() => fetchPokemonDetails(pokemon.url, "left")}>
+                  {pokemon.name.toUpperCase()}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <img src={leftPokemon ? leftPokemon.sprites.front_default : ""} alt="" />
+            <p>{leftPokemon ? leftPokemon.name : "Selecciona un Pokémon"}</p>
+          </div>
+        </div>
+        <div className="right">
+          <ul>
+            {pokemonList.map((pokemon, index) => (
+              <li key={index}>
+                <button onClick={() => fetchPokemonDetails(pokemon.url, "right")}>
+                  {pokemon.name.toUpperCase()}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <img src={rightPokemon ? rightPokemon.sprites.front_default : ""} alt="" />
+            <p>{rightPokemon ? rightPokemon.name : "Selecciona un Pokémon"}</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
